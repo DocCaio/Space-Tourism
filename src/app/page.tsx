6a289b7ffc50167be2a95dab74651b3b0629f888
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2025-02-21 18:41:43 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Header } from "./components";
+import { Router } from "next/router";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <Router>
+      <Header>
+        <Switch>
+          <Route path='/' extract></Route>
+          <Route path='/destination'></Route>
+          <Route path='/crew'></Route>
+          <Route path='/technology'></Route>
 
-    </div>
+        </Switch>
+
+      </Header>
+
+    </Router>
   );
 }
