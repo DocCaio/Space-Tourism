382106d98515ce6711be2e88e68e2cae4d87f402
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2025-02-27 17:10:26 -0300

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import './App.css';
-import { BrowserRouter as Router , Route , Routes  } from 'react-router';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Correct import from 'react-router-dom'
 import { Header, Home } from './components';
-import {Destination , Crew , Technology} from './Pages'
-
-
-
-
+import { Destination, Crew, Technology } from './Pages';
 
 export default function App() {
   return (
-   <Router>
-    <Header/>
-
-    <Routes>
-      <Route path='/'>
-       <Home/>
-      </Route>     
-      <Route path='/destination'></Route>
-      <Destination/>
-      <Route path='/crew'></Route>  
-      <Crew/>    
-      <Route path='/technology'></Route>
-      <Technology/>
-    </Routes>    
-   </Router>
+    <Router>
+      <Header/>
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
+    </Router>
   );
 }
