7026b637d09285aaffdb4f325fbf22c77defe1b2
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2025-03-07 14:17:10 -0300

    up

--- src/Pages/Destination.tsx
+++ src/Pages/Destination.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import { destinations } from "../starter-code/data.json";
+
+
+
 export default function Destination() {
+  const [planets] = useState(destinations)
+  const [value , setValue] =useState(0)
   return (
     <div>
       <h1>Crew page</h1>
