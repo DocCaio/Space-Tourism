217581a44eaff0467a5286cf055817571274589a
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2025-03-10 17:18:55 -0300

    up

--- src/Pages/Destination.tsx
+++ src/Pages/Destination.tsx
@@ -6,9 +6,31 @@ import { destinations } from "../starter-code/data.json";
 export default function Destination() {
   const [planets] = useState(destinations)
   const [value , setValue] =useState(0)
+
+  const {name , images , destinations , distance , travel } =
+  planets[value]
   return (
-    <div>
-      <h1>Crew page</h1>
-    </div>
+    <>
+    <section>
+      <h1>01 Pick your destination</h1>
+
+      <div>
+        <article>
+          <img src={images.png} alt={name} title={name}/>
+        </article>
+
+        <article>
+          <h2>{name}</h2>
+          <p>{destinations}</p>
+          <ul>
+            <li>{distance}</li>
+            <li>{travel}</li>
+          </ul>
+        </article>
+
+      </div>
+
+    </section>
+    </>
   );
 }
