e2815f8b5cb9c48c1a6229c73ab64025817ae8a5
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2025-03-19 13:50:48 -0300

    up

--- src/Pages/Technology.tsx
+++ src/Pages/Technology.tsx
@@ -16,7 +16,33 @@ export default function Destination() {
         <h1 className="pt-20  lg:pt-40 lg:max-w-7xl lg:mx-auto text-white text-4xl uppercase mb-10">03 Space Launch 101</h1>
 
         <div className="flex items-center justify-center flex-col-reverse  md:grid grid-cols-2 gap-5 
-      md: itens-center lg:max-w-7xl lg:mx-auto">
+      md:itens-center  lg:max-w-7xl lg:mx-auto">
+
+      
+
+
+          <article className="text-center lg:text-left lg:flex items-center">
+          <div>
+          {cards.map((item, index) => (
+              <button key={index} onClick={() =>
+                setValue(index)} className={`uppercase
+            text-white text-2xl h-12 w-12 p-2 rounded-full border-white pb-2 m-2
+            ${index === value && "bg-white text-gray-800"}`}>
+                {index + 1}
+
+              </button>
+            ))}
+          </div>
+
+            <div className="ml-10">
+              <h4 className="uppercase text-gray-400 mb-5 text-2xl">The  termonology...</h4>
+
+              <h2 className="text-5xl font-bold mt-10 my-5 uppercase tracking-widest">{name}</h2>
+              <p className="text-gray-400 mb-10">{description}</p>
+
+            </div>
+
+          </article>
 
           <article>
             <picture>
@@ -27,25 +53,6 @@ export default function Destination() {
                 className="block mx-auto mb-10"
               />
             </picture>
-          </article>
-
-
-          <article className="text-center lg:text-left">
-          {cards.map((item, index) => (
-              <button key={index} onClick={() =>
-                setValue(index)} className={`uppercase
-            text-white text-2xl h-12 w-12 p-2 rounded-full   border-white pb-2 mx-2 
-            ${index === value && "bg-white text-gray-800"}`}>
-                {index + 1}
-
-              </button>
-            ))}
-
-
-            <h2 className="text-5xl font-bold mt-10 my-5 uppercase tracking-widest">{name}</h2>
-            <p className="text-gray-400 mb-10">{description}</p>
-          
-
           </article>
 
         </div>
